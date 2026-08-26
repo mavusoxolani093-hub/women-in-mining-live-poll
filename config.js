a7c4dc4db037ee98f3484e87ac56134da1cb11f4
Xolani Mavuso <vxolani777@gmail.com>
2026-08-26 22:32:29 +0200

    Enable dedicated realtime result pages

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.POLL_CONFIG = {
   pollId: "women-in-mining-keynote-2026",
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://ufwcvegttjguyxelqyos.supabase.co",
+  supabaseAnonKey: "sb_publishable_zqi44bdfCw-0YFxGnZsSKA_1YQZTiyu",
   thirdQuestion: "In one word: What is the impact of women in mining?",
   maxWordLength: 36
 };
